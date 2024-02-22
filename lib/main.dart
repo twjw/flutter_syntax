c887bf1793c64e1ca0b3d31229f9ceb41479b3ee
twjw <aaaawjwa@gmail.com>
2024-02-22 13:12:14 +0800

    dio + riverpod FutureProvider 練習

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -9,15 +11,69 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: const MyHomePage(),
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+final dio = Dio();
+
+class AvatarModel {
+  const AvatarModel({required this.src});
+
+  final String src;
+
+  static AvatarModel fromJson(Map<String, dynamic> json) {
+    return AvatarModel(src: json['src']);
+  }
+}
+
+final avatarProvider = FutureProvider<AvatarModel>((ref) async {
+  final response = await dio.get(
+      'https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1',
+      options: Options(
+        headers: {
+          Headers.contentTypeHeader: "application/json",
+          'x-api-key': 'DEMO-API-KEY',
+        },
+        followRedirects: true,
+      ));
+  if (response.statusCode == 200) {
+    return AvatarModel.fromJson({'src': response.data[0]['url']});
+  } else {
+    throw Exception('Failed to fetch data');
+  }
+});
+
+class Avatar extends ConsumerWidget {
+  const Avatar({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final avatar = ref.watch(avatarProvider);
+
+    return avatar.when(
+      // data: (data) => Image.network(data.src),
+      data: (data) => ClipOval(
+        child: Image.asset(
+          'assets/images/avatar.png',
+          width: 78,
+          height: 78,
+          fit: BoxFit.cover,
+        ),
+      ),
+      error: (error, stack) => const Text('error'),
+      loading: () => const SizedBox.shrink(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
+
+  @override
+  void initState() {}
 
   @override
   Widget build(BuildContext context) {
@@ -110,6 +166,8 @@ class MyHomePage extends StatelessWidget {
                           ],
                         ),
                       ),
+                      Spacer(),
+                      Avatar(),
                     ],
                   )
                 ],
